4f1fe990c14276438732916f4590fc2d29491f69
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-16 13:28:14 +0800

    Update XiaoYunwhitelist.lua

--- XiaoYunwhitelist.lua
+++ XiaoYunwhitelist.lua
@@ -6,7 +6,7 @@ until game.Players.LocalPlayer.Character~=nil;
 local a=tostring(game.Players.LocalPlayer.Character);
 if a=="Hudson_From4"then
 _G.whitelisted=true
-elseif a=="Zzz_jkI"then
+elseif a=="COM_7723"then
 _G.whitelisted=true
 end
 if _G.whitelisted==true then
