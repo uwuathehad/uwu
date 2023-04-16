f8660b27a2566259beab29051cb18bec0e4e68e8
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-16 14:09:21 +0800

    Update XiaoYunwhitelist.lua

--- XiaoYunwhitelist.lua
+++ XiaoYunwhitelist.lua
@@ -28,6 +28,8 @@ elseif a=="acti101010"then
 _G.whitelisted=true
 elseif a=="etgdyfgutffff"then
 _G.whitelisted=true
+elseif a=="REDUXDt"then
+_G.whitelisted=true
 end
 if _G.whitelisted==true then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/uwuathehad/uwu/main/XiaoYunV1.0.5.lua"))()
