fdcc519e1cc0855f617a7f3c4bfb7cc1a318b76f
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-16 16:32:04 +0800

    Update XiaoYunwhitelist.lua

--- XiaoYunwhitelist.lua
+++ XiaoYunwhitelist.lua
@@ -6,66 +6,10 @@ until game.Players.LocalPlayer.Character~=nil;
 local a=tostring(game.Players.LocalPlayer.Character);
 if a=="Hudson_From4"then
 _G.whitelisted=true
-elseif a=="COM_7723"then
-_G.whitelisted=true
-elseif a=="123ggwqp1"then
-_G.whitelisted=true
-elseif a=="KBDPSKNSNSNSN"then
-_G.whitelisted=true
-elseif a=="gfhvfva"then
-_G.whitelisted=true
-elseif a=="fafsw3"then
-_G.whitelisted=true
-elseif a=="ss_chara520"then
-_G.whitelisted=true
-elseif a=="aqswde114514"then
-_G.whitelisted=true
-elseif a=="guaz_666"then
-_G.whitelisted=true
-elseif a=="TRYILIJ"then
-_G.whitelisted=true
-elseif a=="jsgajvj"then
-_G.whitelisted=true
-elseif a=="haihaihainb"then
-_G.whitelisted=true
-elseif a=="acti101010"then
-_G.whitelisted=true
-elseif a=="etgdyfgutffff"then
-_G.whitelisted=true
-elseif a=="REDUXDt"then
-_G.whitelisted=true
-elseif a=="whh2500"then
-_G.whitelisted=true
-elseif a=="ffjfjcjid"then
-_G.whitelisted=true
-elseif a=="cccccccc666657"then
-_G.whitelisted=true
-elseif a=="niu_niujun"then
-_G.whitelisted=true
-elseif a=="bbcbbbcv"then
-_G.whitelisted=true
-elseif a=="chemzxcqwer"then
-_G.whitelisted=true
-elseif a=="sj097sk"then
-_G.whitelisted=true
-elseif a=="eeerrrttt22233852"then
-_G.whitelisted=true
-elseif a=="udhehuxh"then
-_G.whitelisted=true
-elseif a=="cxkalm"then
-_G.whitelisted=true
-elseif a=="lipengyinb"then
-_G.whitelisted=true
-elseif a=="yuojklwb"then
-_G.whitelisted=true
-elseif a=="waegfdy"then
-_G.whitelisted=true
-elseif a=="SssGgg_7137H"then
-_G.whitelisted=true
-end
+
 if _G.whitelisted==true then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/uwuathehad/uwu/main/XiaoYunV1.0.5.lua"))()
     else
  
-game.Players.LocalPlayer:Kick(a.."你没有被列入白名单，QQ168777105给免费白名单")
+game.Players.LocalPlayer:Kick("脚本已下架")
 end
