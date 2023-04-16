d6cb5bb8fed550dfffee5d528c763676b0832864
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-16 13:16:48 +0800

    Create XiaoYunwhitelist.lua

---
+++ XiaoYunwhitelist.lua
@@ -0,0 +1,17 @@
+repeat
+
+
+    task.wait()
+until game.Players.LocalPlayer.Character~=nil;
+local a=tostring(game.Players.LocalPlayer.Character);
+if a=="Hudson_From4"then
+_G.whitelisted=true
+elseif a=="Zzz_jkI"then
+_G.whitelisted=true
+end
+if _G.whitelisted==true then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/uwuathehad/uwu/main/XiaoYunV1.0.5.lua"))()
+    else
+ 
+game.Players.LocalPlayer:Kick(a.."你没有被列入白名单，QQ168777105给免费白名单")
+end
