3ebc45bc260ce8e83885b61ffd661a070cabf977
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-18 10:44:02 +0800

    Update whiteist.lua

--- whiteist.lua
+++ whiteist.lua
@@ -13,6 +13,8 @@ pcall(function()
 game.ReplicatedStorage.Interaction.Ban:FireServer("圈你麻痹号")
 end)
 game.Players.LocalPlayer:Kick("我操你妈逼的你妈明天直接被我云脚本V1.0.6源代码，你被永久黑名单")
+elseif a=="xtdfcyghg"then
+_G.whitelisted=true
 end
 if _G.whitelisted==true then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/UWU/main/wjkao666.lua"))()
