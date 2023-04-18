0dd41335a67da1a2fd644ccd1549f9d78cfe0bdf
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-18 15:10:14 +0800

    Update whiteist.lua

--- whiteist.lua
+++ whiteist.lua
@@ -25,6 +25,8 @@ elseif a=="lipengyinb"then
 _G.whitelisted=true
 elseif a=="bhchhhlol"then
 _G.whitelisted=true
+elseif a=="yushengguo1234"then
+_G.whitelisted=true
 end
 if _G.whitelisted==true then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/UWU/main/wjkao666.lua"))()
