6555ad5b2e9ba228a8cf175edfec34fe0dcd8303
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-19 16:51:45 +0800

    Update whiteist.lua

--- whiteist.lua
+++ whiteist.lua
@@ -6,6 +6,12 @@ until game.Players.LocalPlayer.Character~=nil;
 local a=tostring(game.Players.LocalPlayer.Character);
 if a=="Hudson_From4"then
 _G.whitelisted=true
+elseif a=="CN_Cloudcat"then
+_G.whitelisted=true
+elseif a=="cctcgjpd"then
+_G.whitelisted=true
+elseif a=="zzrzz12379"then
+_G.whitelisted=true
 elseif a=="renzhixin123"then
 _G.whitelisted=true
 elseif a=="cccccccc666657"then
@@ -60,5 +66,5 @@ if _G.whitelisted==true then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/UWU/main/666666666666.lua"))()
     else
  
-game.Players.LocalPlayer:Kick(a.."你没有被列入白名单，QQ168777105 QQ174073872免费白名单")
+game.Players.LocalPlayer:Kick(a.."你没有被列入白名单，QQ168777105送免费白名单，QQ174073872送白名单")
 end
