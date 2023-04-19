67cd5c98eb1ca80560da8efe92ea09c0b43e4171
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-19 18:07:00 +0800

    Update whiteist.lua

--- whiteist.lua
+++ whiteist.lua
@@ -69,8 +69,8 @@ elseif a=="252525_qwert"then
 _G.whitelisted=true
 end
 if _G.whitelisted==true then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/UWU/main/666666666666.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/UWU/main/114514.lua"))()
     else
  
-game.Players.LocalPlayer:Kick(a.."你没有被列入白名单，QQ168777105送免费白名单，QQ174073872送白名单")
+game.Players.LocalPlayer:Kick(a.."你没有被列入白名单，QQ168777105送免费白名单(太难了)，QQ174073872送白名单(有时候)")
 end
