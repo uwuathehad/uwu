2138afe78a96442b4de21c6795637b142ff2f106
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-04-22 10:51:37 +0800

    Update whiteist.lua

--- whiteist.lua
+++ whiteist.lua
@@ -6,6 +6,8 @@ until game.Players.LocalPlayer.Character~=nil;
 local a=tostring(game.Players.LocalPlayer.Character);
 if a=="Hudson_From4"then
 _G.whitelisted=true
+elseif a=="eeerrrttt22233852"then
+_G.whitelisted=true
 elseif a=="fhfdhvchfjvuh"then
 _G.whitelisted=true
 elseif a=="54DSBA"then
