40339ff1273a1f9af3374ed3b3c5d160dcaaa16a
uwuathehad <126261103+uwuathehad@users.noreply.github.com>
2023-05-02 14:15:16 +0800

    Update WWWwWW_______WWWWW_____WWWW.lua

--- WWWwWW_______WWWWW_____WWWW.lua
+++ WWWwWW_______WWWWW_____WWWW.lua
@@ -2,102 +2,10 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 local Player = game.Players.LocalPlayer
 local Window = OrionLib:MakeWindow({Name = "☁️", HidePremium = false, SaveConfig = false, IntroEnabled = false})
 
-OrionLib:MakeNotification({
-    Name = "用户",
-    Content = "你已使用 : "..Player.Name.."已登陆 ",
-    Image = "rbxassetid://4483345998",
-    Time = 5
-})
-
-_G.Key = "jala461yun"
-_G.KeyInput = "string"
-
-function MakeScriptHub()
-    print("EnteredCorrectKey")
-end
-
-function CorrectKeyNotification()
-    OrionLib:MakeNotification({
-        Name = "密钥正确",
-        Content = "请稍等!",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-function IncorrectKeyNotification()
-    OrionLib:MakeNotification({
-        Name = "错误",
-        Content = "密钥错误!",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-local Tab = Window:MakeTab({
-	Name = "密钥",
-	Icon = "",
-	PremiumOnly = false
-})
-
-Tab:AddTextbox({
-	Name = "输入密钥",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		_G.KeyInput = Value
-	end	  
-})
-
 Tab:AddButton({
-	Name = "检查密钥!",
+	Name = "Get云脚本V1.1.3",
 	Callback = function()
-      		if _G.KeyInput == _G.Key then
-            MakeScriptHub()
-            CorrectKeyNotification()
-            wait()
-             wait()
-              wait()
-               wait()
-                wait()
-                 wait()
-                  wait()
-                   wait()
-                    wait()
-                     wait()
-                      wait()
-                       wait()
-                        wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/uwuathehad/uwu/main/llllll_______llllll__________llllll.lua"))()	
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-                         wait()
-
-
-                       
-            OrionLib:Destroy()
-            else
-                IncorrectKeyNotification()
-        end
-
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/uwuathehad/uwu/main/HoHo.lua"))()
   	end    
 })
 
@@ -116,7 +24,7 @@ Tab:AddButton({
 })
 
 local Section = Tab:AddSection({
-	Name = "不要密钥"
+	Name = "HoHo"
 })
 
 Tab:AddButton({
